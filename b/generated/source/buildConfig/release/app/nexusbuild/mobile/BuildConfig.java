3be228d9cae20c3a934aa70c0bc34620baee8837
Winston S. Martinez H. <winstons.j.martinez.h@gmail.com>
2026-05-10 11:40:48 -0400

    chore: add remaining local build artifacts

---
+++ b/generated/source/buildConfig/release/app/nexusbuild/mobile/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.nexusbuild.mobile;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "app.nexusbuild.mobile";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 511;
+  public static final String VERSION_NAME = "0.5.11";
+  // Field from default config.
+  public static final boolean IS_EDGE_TO_EDGE_ENABLED = true;
+  // Field from default config.
+  public static final boolean IS_HERMES_ENABLED = true;
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+  // Field from default config.
+  public static final String REACT_NATIVE_RELEASE_LEVEL = "stable";
+}
